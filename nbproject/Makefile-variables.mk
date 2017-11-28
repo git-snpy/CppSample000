0098feabb2d7deb18dc2139e8e1fd6dea42892d9
snpy <iyemon.500ml@gmail.com>
2017-11-28 09:53:15 +0900

    packages updated

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=None-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/None-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=cppsample000
-CND_ARTIFACT_PATH_Debug=dist/Debug/None-Windows/cppsample000
-CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/cppsample000
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=cppsample000.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/cppsample000.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/cppsample000.tar
 # Release configuration
-CND_PLATFORM_Release=None-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/None-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=cppsample000
-CND_ARTIFACT_PATH_Release=dist/Release/None-Windows/cppsample000
-CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/cppsample000
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=cppsample000.tar
-CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/cppsample000.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/cppsample000.tar
 #
 # include compiler specific variables
 #
